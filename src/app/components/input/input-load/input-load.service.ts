2c83ee526536bdbd9dc56e9e03797aa161ccdabc
sasaco <sasaicco@gmail.com>
2021-08-23 13:31:40 +0900

    荷重強度画面の部材荷重の保存に関して 行の有効・無効の判定方法を変えた

--- src/app/components/input/input-load/input-load.service.ts
+++ src/app/components/input/input-load/input-load.service.ts
@@ -490,11 +490,8 @@ export class InputLoadService {
           const P1 = this.helper.toNumber(row["P1"]);
           const P2 = this.helper.toNumber(row["P2"]);
 
-          if (
-            (m1 != null || m2 != null) &&
-            mark != null && //&& direction != ''
-            (L1 != null || L2 != null || P1 != null || P2 != null)
-          ) {
+          if( m1 != null || m2 != null || mark != null || direction != '' ||
+              L1 != null || L2 != null || P1 != null || P2 != null) {
             const tmp = {
               m1: row.m1,
               m2: row.m2,
