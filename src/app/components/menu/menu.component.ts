0e396c6ae08996952dcd7da6bd1a0b9a465a80c6
sasaco <sasaicco@gmail.com>
2021-09-02 18:26:58 +0900

    バージョン上げた

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -49,7 +49,7 @@ export class MenuComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.fileName = "立体骨組構造解析ソフトver1.6.8"
+    this.fileName = "立体骨組構造解析ソフトver1.6.9"
     this.helper.isContentsDailogShow = false;
     this.setDimension(2);
   }
@@ -61,7 +61,7 @@ export class MenuComponent implements OnInit {
     this.InputData.clear();
     this.ResultData.clear();
     this.three.ClearData();
-    this.fileName = "立体骨組構造解析ソフトver1.6.8"
+    this.fileName = "立体骨組構造解析ソフトver1.6.9"
   }
 
   // ファイルを開く
