44156e317472223e4eea81ff37e455b9077aa4da
mizutani488 <g1918082@tcu.ac.jp>
2021-10-25 15:14:01 +0900

    切り替え

--- src/app/components/print/print.component.ts
+++ src/app/components/print/print.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import html2canvas from "html2canvas";
 import { ResultDataService } from "src/app/providers/result-data.service";
 import { ThreeService } from "../three/three.service";
+import { PrintCustomThreeService } from "./custom/print-custom-three/print-custom-three.service";
 import { PrintService } from "./print.service";
 
 @Component({
@@ -10,10 +11,13 @@ import { PrintService } from "./print.service";
   styleUrls: ["./print.component.scss", "../../app.component.scss"],
 })
 export class PrintComponent implements OnInit {
+  public contentEditable2;
+
   constructor(
     public printService: PrintService,
     public ResultData: ResultDataService,
-    private three: ThreeService
+    private three: ThreeService,
+    private customThree: PrintCustomThreeService
   ) {}
 
   ngOnInit(): void {}
@@ -23,6 +27,7 @@ export class PrintComponent implements OnInit {
 
     if (this.printService.contentEditable1[10]) {
       // 図の印刷
+      this.contentEditable2 = this.customThree.contentEditable2;
       this.three.getCaptureImage().then((print_target) => {
         this.printService.print_target = print_target;
         this.printService.printDocument("invoice", [""]);
