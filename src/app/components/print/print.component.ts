d77bb72c16d49d55f2f0b619f713008e7e8ec454
mizutani488 <g1918082@tcu.ac.jp>
2022-01-19 12:02:24 +0900

    名称をjsonデータに入れた

--- src/app/components/print/print.component.ts
+++ src/app/components/print/print.component.ts
@@ -11,7 +11,7 @@ import { DataHelperModule } from "src/app/providers/data-helper.module";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { WaitDialogComponent } from "../wait-dialog/wait-dialog.component";
 import * as pako from "pako";
-import { InputLoadService } from "../input/input-load/input-load.service";
+import { throwIfEmpty } from "rxjs/operators";
 
 @Component({
   selector: "app-print",
@@ -20,9 +20,10 @@ import { InputLoadService } from "../input/input-load/input-load.service";
 })
 export class PrintComponent implements OnInit {
   public contentEditable2;
+  public combineJson: any;
+  public pickupJson: any;
   constructor(
     public InputData: InputDataService,
-    public load: InputLoadService,
     public printService: PrintService,
     public ResultData: ResultDataService,
     private three: ThreeService,
@@ -77,60 +78,74 @@ export class PrintComponent implements OnInit {
         if (
           this.printService.contentEditable1[1] &&
           Object.keys(this.ResultData.disg.disg).length !== 0
-        )
+        ) {
           json["disg"] = this.ResultData.disg.disg;
+          this.getNames(json["disg"], "basic");
+        }
         if (
           this.printService.contentEditable1[2] &&
           Object.keys(this.ResultData.combdisg.disgCombine).length !== 0
-        )
+        ) {
           json["disgCombine"] = this.ResultData.combdisg.disgCombine;
+          this.getNames(json["disgCombine"], "comb");
+        }
         if (
           this.printService.contentEditable1[3] &&
           Object.keys(this.ResultData.pickdisg.disgPickup).length !== 0
-        )
+        ) {
           json["disgPickup"] = this.ResultData.pickdisg.disgPickup;
+          this.getNames(json["disgPickup"], "pick");
+        }
         if (
           this.printService.contentEditable1[7] &&
           Object.keys(this.ResultData.fsec.fsec).length !== 0
-        )
+        ) {
           json["fsec"] = this.ResultData.fsec.fsec;
+          this.getNames(json["fsec"], "basic");
+        }
         if (
           this.printService.contentEditable1[8] &&
           Object.keys(this.ResultData.combfsec.fsecCombine).length !== 0
-        )
+        ) {
           json["fsecCombine"] = this.ResultData.combfsec.fsecCombine;
+          this.getNames(json["fsecCombine"], "comb");
+        }
         if (
           this.printService.contentEditable1[9] &&
           Object.keys(this.ResultData.pickfsec.fsecPickup).length !== 0
-        )
+        ) {
           json["fsecPickup"] = this.ResultData.pickfsec.fsecPickup;
+          this.getNames(json["fsecPickup"], "pick");
+        }
         if (
           this.printService.contentEditable1[4] &&
           Object.keys(this.ResultData.reac.reac).length !== 0
-        )
+        ) {
           json["reac"] = this.ResultData.reac.reac;
+          this.getNames(json["reac"], "reac");
+        }
         if (
           this.printService.contentEditable1[5] &&
           Object.keys(this.ResultData.combreac.reacCombine).length !== 0
-        )
+        ) {
           json["reacCombine"] = this.ResultData.combreac.reacCombine;
+          this.getNames(json["reacCombine"], "comb");
+        }
         if (
           this.printService.contentEditable1[6] &&
           Object.keys(this.ResultData.pickreac.reacPickup).length !== 0
-        )
+        ) {
           json["reacPickup"] = this.ResultData.pickreac.reacPickup;
+          this.getNames(json["reacPickup"], "pick");
+        }
       }
 
       if (Object.keys(json).length === 0) {
         alert("データが存在しないため，印刷できませんでした．");
         return;
-      } else {
-        if (!this.printService.contentEditable1[0]) {
-          json["load"] = this.load.getLoadNameJson(null, "", true);
-        }
-        json["dimension"] = this.helper.dimension;
-        json["input"] = this.printService.contentEditable1[0] ? true : false;
       }
+
+      json["dimension"] = this.helper.dimension;
 
       // resultデータの印刷
       // const blob = new window.Blob([JSON.stringify(json)], {
@@ -170,6 +185,49 @@ export class PrintComponent implements OnInit {
           // finally的な処理
           modalRef.close();
         });
+    }
+  }
+
+  private getNames(jsonData, target) {
+    const keys: string[] = Object.keys(jsonData);
+    this.getNamesAll(target);
+
+    for (const index of keys) {
+      let loadName = "";
+      switch (target) {
+        case "basic":
+          let l: any = this.InputData.load.getLoadNameJson(null, index);
+          if (index in l) {
+            loadName = l[index].name;
+          }
+          break;
+        case "comb":
+          if (index in this.combineJson) {
+            if ("name" in this.combineJson[index]) {
+              loadName = this.combineJson[index].name;
+            }
+          }
+          break;
+        case "pick":
+          if (index in this.pickupJson) {
+            if ("name" in this.pickupJson[index]) {
+              loadName = this.pickupJson[index].name;
+            }
+          }
+          break;
+      }
+      jsonData[index]["name"] = ["Case." + index, loadName];
+    }
+  }
+
+  private getNamesAll(target) {
+    switch (target) {
+      case "comb":
+        this.combineJson = this.InputData.combine.getCombineJson();
+        break;
+      case "pick":
+        this.pickupJson = this.InputData.pickup.getPickUpJson();
+        break;
     }
   }
 
