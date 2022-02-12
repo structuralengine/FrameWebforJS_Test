9b43d9fb4be2fa1c3a10bef6cc34d620242f2e0c
sasaco <sasaicco@gmail.com>
2022-02-12 14:37:08 +0900

    electron win 完成

--- webpack.electron.prod.config.js
+++ webpack.electron.prod.config.js
@@ -1,10 +1,11 @@
+const path = require('path');
 const baseConfig = require('./webpack.electron.config');
 
 module.exports = {
   ...baseConfig,
   mode: 'production',
   output: {
-    path: process.cwd(),
+    path: path.join(process.cwd(), 'dist'),
     filename: 'index.js'
   }
 };
