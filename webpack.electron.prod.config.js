4e22b5155ced068709bc9780c585712e28fc4285
sasaco <sasaicco@gmail.com>
2022-02-12 14:02:46 +0900

    te

--- webpack.electron.prod.config.js
+++ webpack.electron.prod.config.js
@@ -1,11 +1,10 @@
-const path = require('path');
 const baseConfig = require('./webpack.electron.config');
 
 module.exports = {
   ...baseConfig,
   mode: 'production',
   output: {
-    path: path.join(process.cwd(), 'dist'),
+    path: process.cwd(),
     filename: 'index.js'
   }
 };
