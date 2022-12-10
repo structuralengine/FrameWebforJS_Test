8881b5f0523263891c2b69be4bead8d9f45a465f
sasaco <sasaicco@gmail.com>
2022-12-10 12:13:34 +0900

    combine に荷重ケースがない場合で1kb の ピックアップファイルができてしまう #227

--- src/app/components/result/result-pickup-fsec/result-pickup-fsec.service.ts
+++ src/app/components/result/result-pickup-fsec/result-pickup-fsec.service.ts
@@ -159,37 +159,42 @@ export class ResultPickupFsecService {
           }
           continue;
         }
-        for (const k of Object.keys(com)) {
-          const key = k.split('_');
-          const target = com[k];
-          const comparison = tmp[k];
-          for (const id of Object.keys(comparison)) {
-            const a = comparison[id];
-            if (!(id in target)) {
-              continue;
-            }
-            const b = target[id];
-            if (key[1] === 'max') {
-              if (b[key[0]] > a[key[0]]) {
-                tmp[k][id] = com[k][id];
+        if(com==null){
+          console.log('Combine' + combNo + 'がない');
+        } else{
+          for (const k of Object.keys(com)) {
+            const key = k.split('_');
+            const target = com[k];
+            const comparison = tmp[k];
+            for (const id of Object.keys(comparison)) {
+              const a = comparison[id];
+              if (!(id in target)) {
+                continue;
               }
-            } else {
-              if (b[key[0]] < a[key[0]]) {
-                tmp[k][id] = com[k][id];
+              const b = target[id];
+              if (key[1] === 'max') {
+                if (b[key[0]] > a[key[0]]) {
+                  tmp[k][id] = com[k][id];
+                }
+              } else {
+                if (b[key[0]] < a[key[0]]) {
+                  tmp[k][id] = com[k][id];
+                }
               }
             }
-          }
 
-          // 最大値を 集計する
-          for (const value of tmp[k]) {
-            max_value.fx = Math.max(Math.abs(value.fx), max_value.fx);
-            max_value.fy = Math.max(Math.abs(value.fy), max_value.fy);
-            max_value.fz = Math.max(Math.abs(value.fz), max_value.fz);
-            max_value.mx = Math.max(Math.abs(value.mx), max_value.mx);
-            max_value.my = Math.max(Math.abs(value.my), max_value.my);
-            max_value.mz = Math.max(Math.abs(value.mz), max_value.mz);
+            // 最大値を 集計する
+            for (const value of tmp[k]) {
+              max_value.fx = Math.max(Math.abs(value.fx), max_value.fx);
+              max_value.fy = Math.max(Math.abs(value.fy), max_value.fy);
+              max_value.fz = Math.max(Math.abs(value.fz), max_value.fz);
+              max_value.mx = Math.max(Math.abs(value.mx), max_value.mx);
+              max_value.my = Math.max(Math.abs(value.my), max_value.my);
+              max_value.mz = Math.max(Math.abs(value.mz), max_value.mz);
+            }
           }
         }
+
       }
       fsecPickup[pickNo] = tmp;
       max_values[pickNo] = max_value;
