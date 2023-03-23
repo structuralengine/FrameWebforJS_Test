e046d1b6be274335a2c47ccfc9b81acd2dfbce72
yuluji <yuji.hirano@malme.co.jp>
2023-03-23 16:01:44 +0900

    GitHubActions用にenvironmentsフォルダをプッシュ

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: true,
+    firebase: {
+      apiKey: "AIzaSyA_QvA6gsmiCU7fPKeCC4tfTJX6bZjC69w",
+      authDomain: "strcutural-engine.firebaseapp.com",
+      databaseURL: "https://strcutural-engine-default-rtdb.firebaseio.com",
+      projectId: "strcutural-engine",
+      storageBucket: "strcutural-engine.appspot.com",
+      messagingSenderId: "424393809904",
+      appId: "1:424393809904:web:da2eb60ab855cf4e76c735"
+    },
+    calcURL: "https://asia-northeast1-strcutural-engine.cloudfunctions.net/FrameWeb",
+    environment: "LOCAL",
+    loginURL: 'https://platform.structuralengine.com/signup',
+  };
